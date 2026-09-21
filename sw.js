dc354801509678f1c24dd7b67a235d8ff4ed8071
yantotiga1-dot <yanto.tiga.1@gmail.com>
2026-09-21 20:14:41 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='sitampan-voice-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  }).catch(()=>cached)));
+});
